9b8fc0e53436843583d1586966b620427f6e4498
Andrew Ehlers <asehlers17@gmail.com>
2017-12-22 12:14:07 -0500

    added firebase and updated chat based on that

--- assets/js/script_chat.js
+++ assets/js/script_chat.js
@@ -1,3 +1,15 @@
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCqW_av0XGX8OP-LQUWL2BweGy1HpxS2L8",
+  authDomain: "shiftshare-3be77.firebaseapp.com",
+  databaseURL: "https://shiftshare-3be77.firebaseio.com",
+  projectId: "shiftshare-3be77",
+  storageBucket: "shiftshare-3be77.appspot.com",
+  messagingSenderId: "732473847830"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
 // collapsing chat event handlers
 
 function collapseFooter() {
@@ -7,6 +19,7 @@ function collapseFooter() {
   $("#chat").collapse("hide");
 
 }
+
 function showFooter() {
   $("body").removeAttr("class");
   $(".footer2").attr("class","footer footer2");
@@ -20,9 +33,13 @@ function chat() {
 		//code here for adding name of user
 
 		var p = $("<p>");
-		p.text($("#chatText").val());
-        $("#chat").append(p);
-        //code here to send out to firebase
+    var textInput = $("#chatText").val();
+		p.text(textInput);
+    // $("#chat").append(p);
+    //code here to send out to firebase
+    database.ref().set({
+      chat: textInput
+    });
 	}
 	$("#chatText").val("");
 	$("#chat").scrollTop($("#chat")[0].scrollHeight);
@@ -32,7 +49,6 @@ function chat() {
 // Triggers for collapsing again: after 5 seconds, it collapses if not hovering
 
 // $(".footer").mouseenter( showFooter()).mouseleave( collapseFooter() );
-
 
 $("#chat").bind("DOMSubtreeModified", function() {
   showFooter();
@@ -53,4 +69,14 @@ $( ".footer" ).hover(
     collapseFooter();
   }
 );
+
+database.ref().on("value", function(snapshot){
+  if(snapshot.val() == null){
+  }
+  else{
+    $("#chat").append("<p>"+snapshot.val().chat + "</p>");
+  }
+},function(error){
+    console.log(error);
+});
 
