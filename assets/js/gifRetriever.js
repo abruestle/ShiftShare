cef0f3f97e6ab15004788eb19612704481701426
abruestle <anna@bruestle.net>
2018-01-02 14:10:58 -0500

    buttons and progress bar for gifs fixed; clicking anywhere on headings opens section

--- assets/js/gifRetriever.js
+++ assets/js/gifRetriever.js
@@ -28,7 +28,7 @@ var gifRetriever = {
 	    }).done(function(response) {
 	    	var image_url = response.data.image_url.replace("/giphy.gif", "");
 
-	    	$("#images").prepend('<div class="card"><img class="card-img-top img-fluid" src="'+ image_url +'/200w_s.gif" data-still = "'+ image_url +'/200w_s.gif" data-animate = "'+ image_url +'/200w_d.gif" data-state = "still" class="gif"><div class="card-block"><div class="row justify-content-center"><div class="col-md-2"><button type="button" class="btn btn-primary btn-sm">Share</button></div><div class="col-md-1"></div><div class="col-md-9"><div class="progress"><div class="progress-bar" style="width:0%"></div></div></div></div></div></div>');
+	    	$("#images").prepend('<div class="card"><img class="card-img-top img-fluid" src="'+ image_url +'/200w_s.gif" data-still = "'+ image_url +'/200w_s.gif" data-animate = "'+ image_url +'/200w_d.gif" data-state = "still" class="gif"><div class="card-block"><div class="row justify-content-center"><div class="col-md-2"><button type="button" class="btn btn-primary btn-sm shift" value="'+ image_url +'">Shift!</button></div><div class="col-md-2"><button type="button" class="btn btn-primary btn-sm share" value="'+ image_url +'">Share!</button></div><div class="col"><div class="progress"><div class="progress-bar" style="width:0%"></div></div></div></div></div></div>');
 	    });
 
 	},
@@ -38,7 +38,7 @@ var gifRetriever = {
 		for (var i = 0; i < gifRetriever.topics[gifRetriever.topicCategories.indexOf((gifRetriever.curCategory).toLowerCase())].length; i++) {
 			$("#categoryTopics").append(gifRetriever.buttonCreator(gifRetriever.topics[gifRetriever.topicCategories.indexOf((gifRetriever.curCategory).toLowerCase())][i], "CategoryTopics"));
 		}
-		$("#headingCategoryTopics").text(gifRetriever.toTitleCase(gifRetriever.curCategory));
+		$("#headingCategoryTopics a").text(gifRetriever.toTitleCase(gifRetriever.curCategory));
 		$("#collapseMyTopics").collapse("hide");
 		$("#collapseCategoryTopics").collapse("show");
 		$("#collapseGifEffects").collapse("hide");
@@ -104,6 +104,7 @@ $("body").on("click", "#addTopic", function() {
 	}
 	$("#newTopic").val("");
 });
+
 
 
 $("body").on("click", "#clearTopics", function() {
